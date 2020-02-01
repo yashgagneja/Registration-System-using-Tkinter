75da22481ad9b066a8974c9262b846b89fd8114c
yashgagneja <56828872+yashgagneja@users.noreply.github.com>
2020-02-01 16:12:01 +0530

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from tkinter import *
+
+def register_user():
+    username_info=username.get()
+    password_info=password.get()
+    file=open(username_info+".txt","w")
+    file.write(username_info+"\n")
+    file.write(password_info)
+    file.close()
+    
+    username_entry.delete(0, END)
+    password_entry.delete(0,END)
+    
+    Label(screen1,text=" Registration Successful").pack()
+def login():
+    print("wait for sometime")
+
+def register():
+    global screen1
+    screen1= Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+    global username
+    global password
+    global username_entry
+    global password_entry
+    
+    username = StringVar()
+    password = StringVar()
+    Label(screen1,text="please enter details").pack()
+    Label(screen1,text="").pack()
+    Label(screen1,text = "Username").pack()
+    username_entry=Entry(screen1,textvariable = username)
+    username_entry.pack()
+    Label(screen1,text = "Password").pack()
+    password_entry=Entry(screen1,textvariable = password)
+    password_entry.pack()
+    Label(screen1,text="").pack()
+    Button(screen1, text="Register",command=register_user).pack()
+    
+def main_screen():
+    global screen
+    screen=Tk()
+    screen.geometry("300x250")
+    screen.title("Notes 1.0")
+    Label(text = "Notes 1.0",bg = "grey", width = "300", font = ("Calibry",13)).pack()
+    Label(text="").pack()
+    Button(text="Login", height = "2", width="30",command = login).pack()
+    Label(text="").pack()
+    Button(text= "Register",height="2",width= "30",command = register).pack()
+    
+    screen.mainloop()
+main_screen()
